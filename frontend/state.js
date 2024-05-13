d7555e9a3e84af59e429f360add2fbabc6c2f478
Hclothilde <160497016+Hclothilde@users.noreply.github.com>
2024-05-13 10:59:22 +0200

    Add files via upload

--- frontend/state.js
+++ frontend/state.js
@@ -1,5 +1,3 @@
-// This variable holds the URL of the server where the backend is hosted
-let serverUrl = "http://10.42.0.1:5000";
 // Set up an interval to periodically fetch and update the state information
 setInterval(async function () {
   // Fetch the state information from the server
