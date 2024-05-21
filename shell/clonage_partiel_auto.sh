d23c9d002081322f1144f9e8c7523e07d7dd5a77
Hclothilde <160497016+Hclothilde@users.noreply.github.com>
2024-05-21 11:36:55 +0200

    Update clonage_partiel_auto.sh

--- shell/clonage_partiel_auto.sh
+++ shell/clonage_partiel_auto.sh
@@ -1,9 +1,9 @@
-cd /home/ifremerdev/Documents
-mkdir software_vlatest
-cd software_vlatest
-git init
-git remote add -f origin https://github.com/Hclothilde/Test.git
-git sparse-checkout init
-git sparse-checkout set "software"
-git sparse-checkout list
-git pull origin main
+cd /home/ifremerdev/Documents #go to the local folder Document
+mkdir software_vlatest #create a new local folder
+cd software_vlatest #go into this new folder
+git init #initialiase it as a git
+git remote add -f origin https://github.com/Hclothilde/Test.git #link it to the following git
+git sparse-checkout init #initialise the partial cloning
+git sparse-checkout set "software" #set the path of the document we want to clone, clone only this document
+git sparse-checkout list #check the list of the cloned folder
+git pull origin main #pull the version of the folder which is on the main branch
